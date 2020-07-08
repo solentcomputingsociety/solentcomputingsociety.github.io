62b499792cc31d5449efbaaa9c2573e1b66acc8a
Bradley Marshall <bradm499@gmail.com>
2020-07-08 17:20:03 +0100

    fixes to notification handler

--- s/cript/app.js
+++ s/cript/app.js
@@ -142,7 +142,7 @@ console.info("\nSolent\nComputing\nSociety_\n\n\nA message to the society member
 		subscribe = subscribe || false;
 		if (!subscribe){
 			firebaseMessaging.getToken().then((token) => firebaseMessaging.deleteToken(token)).then(() => {
-				firebase.firestore().collection("users/members/id/" + firebase.auth().currentUser.uid).doc(token).delete().catch(function(error){
+				firebase.firestore().collection("users/members/id/" + firebase.auth().currentUser.uid + "/tokens").doc(token).delete().catch(function(error){
 					console.warn("error-delete:",error);
 				});
 			}).catch((error) => {
@@ -154,7 +154,7 @@ console.info("\nSolent\nComputing\nSociety_\n\n\nA message to the society member
 	}
 	function subscribeToNotificationsRefresh(){
 		firebaseMessaging.getToken().then((token) => {
-			firebase.firestore().collection("users/members/id/" + firebase.auth().currentUser.uid).doc(token).set({
+			firebase.firestore().collection("users/members/id/" + firebase.auth().currentUser.uid + "/tokens").doc(token).set({
 				notificationToken: token
 			}).catch(function(error){
 				console.warn("token-refresh:",error);
