170035956cf7f00b29525d29c15e5794700a5bf3
Bradley Marshall <bradm499@gmail.com>
2020-07-08 17:13:21 +0100

    fixes to notification handler

--- s/cript/app.js
+++ s/cript/app.js
@@ -142,20 +142,22 @@ console.info("\nSolent\nComputing\nSociety_\n\n\nA message to the society member
 		subscribe = subscribe || false;
 		if (!subscribe){
 			firebaseMessaging.getToken().then((token) => firebaseMessaging.deleteToken(token)).then(() => {
-				firebase.firestore().collection("users/members/id/" + firebase.auth().currentUser.uid).doc(token).delete().catch(function(error){
-					
+				firebase.firestore().doc("users/members/id/" + firebase.auth().currentUser.uid + "/" + token).delete().catch(function(error){
+					console.warn("error-delete:",error);
 				});
-			}).catch(() => {});
+			}).catch((error) => {
+				console.warn("error-delete-token:",error);
+			});
 		} else {
 			firebaseMessaging.requestPermission().then(() => subscribeToNotificationsRefresh()).catch(()=> {});
 		}
 	}
 	function subscribeToNotificationsRefresh(){
 		firebaseMessaging.getToken().then((token) => {
-			firebase.firestore().collection("users/members/id/" + firebase.auth().currentUser.uid).doc(token).set({
+			firebase.firestore().doc("users/members/id/" + firebase.auth().currentUser.uid + "/" + token).set({
 				notificationToken: token
 			}).catch(function(error){
-				
+				console.warn("token-refresh:",error);
 			});
 		});
 	}
