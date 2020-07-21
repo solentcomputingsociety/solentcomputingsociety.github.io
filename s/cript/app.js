7a03e9eaec14572d05006f19e85bc81e92e00039
Bradley Marshall <bradm499@gmail.com>
2020-07-21 17:26:13 +0100

    Updated index page and used higher quality images

--- s/cript/app.js
+++ s/cript/app.js
@@ -6,10 +6,16 @@ console.info("\nSolent\nComputing\nSociety_\n\n\nA message to the society member
 		location.assign("/error/page_ref");
 		return;
 	}
-	var scripts = ["https://www.gstatic.com/firebasejs/7.15.0/firebase-app.js","https://www.gstatic.com/firebasejs/7.15.0/firebase-firestore.js","https://www.gstatic.com/firebasejs/7.15.0/firebase-auth.js"];
+	var scripts = ["https://www.gstatic.com/firebasejs/7.15.0/firebase-app.js","https://www.gstatic.com/firebasejs/7.15.0/firebase-firestore.js"];
 	if (page_ref == "d2a57dc1d883fd21fb9951699df71cc7"){
 		scripts.push("https://www.gstatic.com/firebasejs/7.15.0/firebase-database.js");
 		scripts.push("https://www.gstatic.com/firebasejs/7.15.0/firebase-storage.js");
+	}
+	if (page_ref == "106A6C241B8797F52E1E77317B96A201"){
+		scripts.push("https://www.gstatic.com/firebasejs/7.15.0/firebase-storage.js");
+
+	} else {
+		scripts.push("https://www.gstatic.com/firebasejs/7.15.0/firebase-auth.js");
 	}
 	var load_remains = scripts.length;
 	for (var i = 0; i < scripts.length; i++) {
@@ -367,6 +373,79 @@ console.info("\nSolent\nComputing\nSociety_\n\n\nA message to the society member
 			}
 		} catch(e) {}
 		switch (page) {
+			case "106A6C241B8797F52E1E77317B96A201":
+				if (!preload){
+					document.getElementById("hero_scroll_more").addEventListener("click",function(){
+						window.scroll({behavior:"smooth",left:0,top:window.innerHeight});
+					});
+					var imgs = ["img/local/scs_1.jpg","img/local/scs_2.jpg","img/local/scs_3.jpg","img/local/scs_4.jpg","img/local/scs_5.jpg","img/local/scs_6.jpg","img/local/scs_7.jpg","img/local/scs_9.jpg","img/local/scs_11.jpg"];
+					(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))imgs=["img/local/scs_1.jpg","img/local/scs_2.jpg","img/local/scs_4.jpg","img/local/scs_5.jpg","img/local/scs_6.jpg","img/local/scs_8.jpg","img/local/scs_10.jpg"]})(navigator.userAgent||navigator.vendor||window.opera);
+					document.getElementById("hero_container").style.backgroundImage = "url(\"" + imgs[Math.floor(Math.random() * imgs.length)] + "\")";
+					await firebase.firestore().collection("users").doc("positions").get().then(function(positions_data){
+						var positions = positions_data.data();
+						if (positions["president"] != null){
+							firebase.firestore().collection("users/members/id").doc(positions["president"]).get().then(async function(user){
+								var user = user.data();
+								if (user.photo.length > 0){
+									user.photo = await storage_download("profile/"+user.photo);
+									document.getElementById("president_image").style.backgroundImage = "url(\"" + user.photo + "\")";
+								}
+								document.getElementById("president_name").innerText = user.name;
+							});
+						}else {
+							document.getElementById("president_name").parentNode.remove();
+						}
+						if (positions["vice-president"] != null){
+							firebase.firestore().collection("users/members/id").doc(positions["vice-president"]).get().then(async function(user){
+								var user = user.data();
+								if (user.photo.length > 0){
+									user.photo = await storage_download("profile/"+user.photo);
+									document.getElementById("vice-president_image").style.backgroundImage = "url(\"" + user.photo + "\")";
+								}
+								document.getElementById("vice-president_name").innerText = user.name;
+							});
+						}else {
+							document.getElementById("vice-president_name").parentNode.remove();
+						}
+						if (positions["treasurer"] != null){
+							firebase.firestore().collection("users/members/id").doc(positions["treasurer"]).get().then(async function(user){
+								var user = user.data();
+								if (user.photo.length > 0){
+									user.photo = await storage_download("profile/"+user.photo);
+									document.getElementById("treasurer_image").style.backgroundImage = "url(\"" + user.photo + "\")";
+								}
+								document.getElementById("treasurer_name").innerText = user.name;
+							});
+						}else {
+							document.getElementById("treasurer_name").parentNode.remove();
+						}
+						if (positions["events_organiser"] != null){
+							firebase.firestore().collection("users/members/id").doc(positions["events_organiser"]).get().then(async function(user){
+								var user = user.data();
+								if (user.photo.length > 0){
+									user.photo = await storage_download("profile/"+user.photo);
+									document.getElementById("events_organiser_image").style.backgroundImage = "url(\"" + user.photo + "\")";
+								}
+								document.getElementById("events_organiser_name").innerText = user.name;
+							});
+						}else {
+							document.getElementById("events_organiser_name").parentNode.remove();
+						}
+						if (positions["social_secretary"] != null){
+							firebase.firestore().collection("users/members/id").doc(positions["social_secretary"]).get().then(async function(user){
+								var user = user.data();
+								if (user.photo.length > 0){
+									user.photo = await storage_download("profile/"+user.photo);
+									document.getElementById("social_secretary_image").style.backgroundImage = "url(\"" + user.photo + "\")";
+								}
+								document.getElementById("social_secretary_name").innerText = user.name;
+							});
+						}	else {
+							document.getElementById("social_secretary_name").parentNode.remove();
+						}
+					});
+				}
+				break;
 			case "d56b699830e77ba53855679cb1d252da":
 				var login_panel;
 				var load_spinner;
@@ -3629,9 +3708,5 @@ console.info("\nSolent\nComputing\nSociety_\n\n\nA message to the society member
 				return;
 		}
 	}
-	if (page_ref == "d2a57dc1d883fd21fb9951699df71cc7"){
-		host_page(page_ref,true);
-	} else {
-		host_page(page_ref,true);
-	}
+	host_page(page_ref,true);
 })();
